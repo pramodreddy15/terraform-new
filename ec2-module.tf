97e39f93795133b7b09a528c29e63f08a8e5a50a
pramodreddy15 <b.pramod15@gmail.com>
2025-09-30 17:41:44 -0400

    this is my first commit

---
+++ ec2-module.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
+data "aws_ami" "my_image"{
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = data.aws_ami.my_image.id
+  instance_type = "t2.micro"
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
